a81e92557e23a31b6f6d74559f9e62b5c4442757
Brian <brian@computers-MacBook-Pro.local>
2023-03-31 16:50:26 -0700

    added first question

--- scripts.js
+++ scripts.js
@@ -1,26 +1,58 @@
+var GuessQuestionOne = 'What does MS stand for in "MS-Dos"?'
+
+var GuessAnswerOne = "Miss. That's her personal title"
+
+var GuessAnswerTwo = "Microsoft"
+
+var GuessAnswerThree = "Micro Shaft"
+
+var GuessAnswerFour = "Mono Sodium"
+
+
+
+
+
+
+
+
+
+
 questionBoxSelector = document.getElementById('questionBox');
 answerBoxSelector = document.getElementById('answerBox')
 
 
 var firstQuestion = document.createElement('li')
 firstQuestion.setAttribute("class","questions")
-firstQuestion.textContent = "First Question"
-
-var secondQuestion = document.createElement('li')
-secondQuestion.setAttribute("class","questions")
-secondQuestion.textContent = "Second Question"
-
-
+firstQuestion.textContent = GuessQuestionOne
 questionBoxSelector.appendChild(firstQuestion)
-questionBoxSelector.appendChild(secondQuestion)
+
+// var secondQuestion = document.createElement('li')
+// secondQuestion.setAttribute("class","questions")
+// secondQuestion.textContent = "Second Question"
+// questionBoxSelector.appendChild(secondQuestion)
 
 var firstAnswer = document.createElement('li')
-
 firstAnswer.setAttribute("class","answer")
-firstAnswer.textContent = "First Answer"
-
-var firstAnswer = document.createElement('li')
-firstAnswer.setAttribute("class","answer")
-firstAnswer.textContent = "First Answer"
+firstAnswer.textContent = GuessAnswerOne
 
 answerBoxSelector.appendChild(firstAnswer)
+
+
+var secondAnswer = document.createElement('li')
+secondAnswer.setAttribute("class","answer")
+secondAnswer.textContent = GuessAnswerTwo
+
+answerBoxSelector.appendChild(secondAnswer)
+
+
+var thirdAnswer = document.createElement('li')
+thirdAnswer.setAttribute("class","answer")
+thirdAnswer.textContent = GuessAnswerThree
+
+answerBoxSelector.appendChild(thirdAnswer)
+
+var forthAnswer = document.createElement('li')
+forthAnswer.setAttribute("class","answer")
+forthAnswer.textContent = GuessAnswerFour
+
+answerBoxSelector.appendChild(forthAnswer)
