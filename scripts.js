4018d827aaefeb0b50057017385a0547ae74d824
Brian <brian@computers-MacBook-Pro.local>
2023-03-31 17:38:51 -0700

    buttons for all first questions

--- scripts.js
+++ scripts.js
@@ -1,3 +1,6 @@
+var PageNumberVaribale = 1
+
+
 var GuessQuestionOne = 'What does MS stand for in "MS-Dos"?'
 
 var GuessAnswerOne = "Ms. That's her personal title"
@@ -7,6 +10,7 @@ var GuessAnswerTwo = "Microsoft"
 var GuessAnswerThree = "Micro Shaft"
 
 var GuessAnswerFour = "Mono Sodium"
+
 
 
 
@@ -31,29 +35,38 @@ questionBoxSelector.appendChild(firstQuestion)
 // secondQuestion.textContent = "Second Question"
 // questionBoxSelector.appendChild(secondQuestion)
 
+
+var answerOneButton = document.createElement("button");
+answerOneButton.innerHTML = GuessAnswerOne;
 var firstAnswer = document.createElement('li')
 firstAnswer.setAttribute("class","answer")
 firstAnswer.textContent = GuessAnswerOne
 
-answerBoxSelector.appendChild(firstAnswer)
+answerBoxSelector.appendChild(answerOneButton)
 
 
+
+var answerTwoButton = document.createElement("button");
+answerTwoButton.innerHTML = GuessAnswerTwo;
 var secondAnswer = document.createElement('li')
 secondAnswer.setAttribute("class","answer")
 secondAnswer.textContent = GuessAnswerTwo
 
-answerBoxSelector.appendChild(secondAnswer)
+answerBoxSelector.appendChild(answerTwoButton)
 
-
+var answerThreeButton = document.createElement("button");
+answerThreeButton.innerHTML = GuessAnswerThree;
 var thirdAnswer = document.createElement('li')
 thirdAnswer.setAttribute("class","answer")
 thirdAnswer.textContent = GuessAnswerThree
 
-answerBoxSelector.appendChild(thirdAnswer)
+answerBoxSelector.appendChild(answerThreeButton)
 
+var answerFourButton = document.createElement("button");
+answerFourButton.innerHTML = GuessAnswerFour;
 var forthAnswer = document.createElement('li')
 forthAnswer.setAttribute("class","answer")
 forthAnswer.textContent = GuessAnswerFour
 
-answerBoxSelector.appendChild(forthAnswer)
+answerBoxSelector.appendChild(answerFourButton)
 });
